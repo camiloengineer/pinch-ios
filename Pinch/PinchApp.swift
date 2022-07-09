1616d57bf459389506dd3eb93439f0394dc27956
Camilo González <cgonzalezm1234@gmail.com>
2022-07-09 12:01:25 -0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Camilo Gonzalez on 09-07-22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
